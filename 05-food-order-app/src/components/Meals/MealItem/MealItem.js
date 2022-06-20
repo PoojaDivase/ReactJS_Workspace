aed19aac2f6900bea5e155cf127806dc999b23cd
Pooja Divase <divasepooja@gmail.com>
2022-06-20 21:37:51 +0530

    V5 MealItemForm commit

--- 05-food-order-app/src/components/Meals/MealItem/MealItem.js
+++ 05-food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 
 const MealItem = props => {
@@ -12,8 +13,7 @@ const MealItem = props => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-
-
+                <MealItemForm id={props.id} />
             </div>
         </li>
     );
