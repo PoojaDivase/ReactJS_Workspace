1bff8b60172b89b6b13b330e61cb8307a7951638
Pooja Divase <divasepooja@gmail.com>
2022-06-20 16:22:24 +0530

    Input class

--- 04-side-effects-demo/src/components/Login/Login.js
+++ 04-side-effects-demo/src/components/Login/Login.js
@@ -4,6 +4,7 @@ import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 import AuthContext from '../../store/AuthContext';
+import Input from '../UI/Input/input';
 
 const emailReducer = (state, action) => {
     if (action.type === "USER_INPUT") {
@@ -61,25 +62,25 @@ const Login = (props) => {
     const { isValid: passwordValid } = passwordState;
 
     //UseEffect() ==>
-      //useeffect is to handle sideeffect
-      //checking and updating state in response of user input in username and password
-      //re-render whenever specified dependecies changed insted of rendering code all the time
-      //[] -> will render only once ...that is initial time
-      useEffect(() => {
-          const identifier = setTimeout(() => {
-              console.log("Checking Form Validity");
-              setFormIsValid(
-                  emailState.isValid && passwordState.isValid
-              );
-          }, 200);  //500 milliseconds
-  
-          /// we can return function from useEffect 
-          // it will run before new sideeffect function runs and does not runs at 1 run of sideeffect function
-          return () => {
-              console.log("CLEAN_UP");
-              clearTimeout(identifier);
-          };
-      }, [emailValid, passwordValid]); // setFormIsValid function as a dependency can be added
+    //useeffect is to handle sideeffect
+    //checking and updating state in response of user input in username and password
+    //re-render whenever specified dependecies changed insted of rendering code all the time
+    //[] -> will render only once ...that is initial time
+    useEffect(() => {
+        const identifier = setTimeout(() => {
+            console.log("Checking Form Validity");
+            setFormIsValid(
+                emailState.isValid && passwordState.isValid
+            );
+        }, 200);  //500 milliseconds
+
+        /// we can return function from useEffect 
+        // it will run before new sideeffect function runs and does not runs at 1 run of sideeffect function
+        return () => {
+            console.log("CLEAN_UP");
+            clearTimeout(identifier);
+        };
+    }, [emailValid, passwordValid]); // setFormIsValid function as a dependency can be added
 
     const emailChangeHandler = (event) => {
         //setEnteredEmail(event.target.value);
@@ -117,32 +118,23 @@ const Login = (props) => {
     return (
         <Card className={classes.login}>
             <form onSubmit={submitHandler}>
-                <div
-                    className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
-                        }`}
-                >
-                    <label htmlFor="email">E-Mail</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={emailState.value}
-                        onChange={emailChangeHandler}
-                        onBlur={validateEmailHandler}
-                    />
-                </div>
-                <div
-                    className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''
-                        }`}
-                >
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        value={passwordState.value}
-                        onChange={passwordChangeHandler}
-                        onBlur={validatePasswordHandler}
-                    />
-                </div>
+                <Input
+                    id="email"
+                    label="email"
+                    type="email"
+                    isValid={emailIsValid}
+                    value={emailState.value}
+                    onChange={emailChangeHandler}
+                    onBlur={validateEmailHandler} />
+                <Input
+                    label="password"
+                    type="password"
+                    id="password"
+                    isValid={passwordIsValid}
+                    value={passwordState.value}
+                    onChange={passwordChangeHandler}
+                    onBlur={validatePasswordHandler}
+                />
                 <div className={classes.actions}>
                     <Button type="submit" className={classes.btn} disabled={!formIsValid}>
                         Login
