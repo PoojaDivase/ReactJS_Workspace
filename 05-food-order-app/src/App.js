44f4ac3980adeeddd4df6da5703c86df191eb5f8
Pooja Divase <divasepooja@gmail.com>
2022-06-20 19:21:17 +0530

    V2 header and HeaderCartButton.js

--- 05-food-order-app/src/App.js
+++ 05-food-order-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import React, {Fragment} from 'react';
 import './App.css';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment> 
+      <Header></Header>
+      </Fragment>
   );
 }
 
