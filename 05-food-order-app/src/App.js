fa4ce8d1cbfb30a4b1cf4a599d07b5c3de3c1ef7
Pooja Divase <divasepooja@gmail.com>
2022-06-20 19:51:16 +0530

    V3 Meals components

--- 05-food-order-app/src/App.js
+++ 05-food-order-app/src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <Fragment> 
+    <Fragment>
       <Header></Header>
-      </Fragment>
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
