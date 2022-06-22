6ed15f54a5f779141045362fcefd5ec8681eddf0
Pooja Divase <divasepooja@gmail.com>
2022-06-22 21:16:16 +0530

    v2 - working of react

--- 06-a-look-behind-the-scenes/src/App.js
+++ 06-a-look-behind-the-scenes/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log("App Running...!!!!!");
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(preShowParagraph => !preShowParagraph);
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p> This is New ...!!!</p>}
+      <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
 }
