a41c2dc2b5596497c3b2d387846b61ec789f2b54
Pooja Divase <divasepooja@gmail.com>
2022-07-02 14:54:25 +0530

    V2 - working with forms and getting input from form

--- 10-working-with-forms/src/components/SimpleInput.js
+++ 10-working-with-forms/src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+
+    const nameInputRef = useRef();
+    const [enteredName, setEnteredName] = useState("");
+
+    const nameInputChangeHandler = event => {
+        setEnteredName(event.target.value);
+    }
+
+    const formSubmissionHandler = (event) => {
+        event.preventDefault(); // no server - so we dont want http server call here- avoiding default behaviour
+        console.log("Name: " ,enteredName);
+        setEnteredName("");
+
+        //if we want to use value once then useRef is good- no need to update state
+        const enteredValue = nameInputRef.current.value;//ref is obj with current property
+        console.log("Name Ref : ",enteredValue);
+        nameInputRef.current.value= ""; //NOT IDEAL, DONT manipulate the DOM
+    }
+
     return (
-      <form>
+      <form onSubmit={formSubmissionHandler}>
         <div className='form-control'>
           <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
+          <input 
+          ref={nameInputRef} 
+          type='text' 
+          id='name' 
+          onChange={nameInputChangeHandler}
+          value={enteredName}/>
         </div>
         <div className="form-actions">
           <button>Submit</button>
