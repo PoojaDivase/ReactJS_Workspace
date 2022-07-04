4b2ccb1c52c1cb7847bc795745146360c04a8011
Pooja Divase <divasepooja@gmail.com>
2022-07-04 22:58:07 +0530

    v3- use redux to get toggle value

--- 11-diving-into-redux/src/components/Counter.js
+++ 11-diving-into-redux/src/components/Counter.js
@@ -3,11 +3,15 @@ import { useDispatch, useSelector, connect } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-    const toggleCounterHandler = () => { };
-
+    
     //useStore and useSelector 
     const counter = useSelector(state => state.counter);
     const dispatch = useDispatch(); //dispatch function -> call to dispatch action against redux store
+    const showCounter = useSelector(state => state.showCounter);
+
+    const toggleCounterHandler = () => {
+        dispatch({type : 'toggle'});
+     };
 
     const incrementHandler = () => {
         dispatch({type : 'increment'});
@@ -17,12 +21,17 @@ const Counter = () => {
         dispatch({type : 'decrement'});
     }
 
+    const increaseHandler = () => {
+        dispatch({type : 'increase', amount: 5});
+    }
+
     return (
         <main className={classes.counter}>
             <h1>Redux Counter</h1>
-            <div className={classes.value}>{counter} </div>
+            {showCounter && <div className={classes.value}>{counter} </div>}
             <div>
                 <button onClick={incrementHandler }>Increment</button>
+                <button onClick={increaseHandler }>Increase By 5</button>
                 <button onClick={ decrementHandler}>Decrement</button>
             </div>
             <br />
