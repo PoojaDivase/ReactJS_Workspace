e39ff39fc4a087f9745bb0eff6f7302225983b99
Pooja Divase <divasepooja@gmail.com>
2022-07-20 21:47:54 +0530

    react Router

---
+++ 13-react-router-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <p>Lets get Started</p>
+  );
+}
+
+export default App;
+
+//our-domain.com/ => Component A
+//our-domain.com/products => Component A
