8a2057128ef491f111e06f7d6af831dc6293ec4f
Pooja Divase <divasepooja@gmail.com>
2022-07-25 20:46:07 +0530

    creative nested routes

--- 14-react-router-app-practice/src/pages/QuoteDetail.js
+++ 14-react-router-app-practice/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
 
@@ -8,24 +8,32 @@ const DUMMY_QUOTES = [
     { id: "q2", author: "Pooja", text: "Pooja's Comment" },
 ];
 
-
 const QuoteDetail = () => {
-
+    const match = useRouteMatch();
     const params = useParams();
-    console.log("Quote ID : ", params.quoteId)
-
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+  
+    const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  
     if (!quote) {
-        return <p> No Quote Found...!!!!!</p>
+      return <p>No quote found!</p>;
     }
+  
     return (
-        <Fragment>
-            <HighlightedQuote text={quote.text} author={quote.author} />
-            <Route path={`/quotes/${params.quoteId}/comments`} exact>
-                <Comments />
-            </Route>
-        </Fragment>
+      <Fragment>
+        <HighlightedQuote text={quote.text} author={quote.author} />
+        <Route path={match.path} exact>
+          <div className='centered'>
+            <Link className='btn--flat' to={`${match.url}/comments`}>
+              Load Comments
+            </Link>
+          </div>
+        </Route>
+        <Route path={`${match.path}/comments`}>
+          <Comments />
+        </Route>
+      </Fragment>
     );
-}
-
-export default QuoteDetail;
+  };
+  
+  export default QuoteDetail;
+  
